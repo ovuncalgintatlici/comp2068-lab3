dee477ad9effdd46cf0dab7af4cfcfd5997f4f05
ovuncalgintatlici <43188371+ovuncalgintatlici@users.noreply.github.com>
2019-10-02 23:13:26 -0400

    first commit

---
+++ lab3/routes/index.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/ovunc', function (req, res) {
+    res.render('ovunc', { title: 'Express' });
+});
+
+router.get('/tamer', function (req, res) {
+    res.render('tamer', { title: 'Express' });
+});
+
+
+router.get('/ethel', function (req, res) {
+    res.render('ethel', { title: 'Express' });
+});
+
+
+
+module.exports = router;
